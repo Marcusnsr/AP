842fcbab5bdae6635ff2993aa57aae19d46f91f6
Marcusnsr <mnsr@hotmail.dk>
2024-09-20 15:19:42 +0200

    done with tests for 2 and 3

--- week2/a2-handout/src/APL/Eval_Tests.hs
+++ week2/a2-handout/src/APL/Eval_Tests.hs
@@ -80,10 +80,64 @@ evalTests =
           @?= Right (ValBool True)
     ]
 
+printTest :: TestTree
+printTest =
+  testGroup
+    "Print testing"
+    [ testCase "Print int" $
+        fst (eval' (Print "x" (CstInt 42)))
+          @?= ["x: 42"],
+      --
+      testCase "Print boolean True" $
+        fst (eval' (Print "flag" (CstBool True)))
+          @?= ["flag: True"],
+    --
+      testCase "Print division by zero failure" $
+        fst (eval' (Print "div" (Div (CstInt 42) (CstInt 0))))
+          @?= [],
+    --
+      testCase "Print sum of two integers" $
+        fst (eval' (Print "sum" (Add (CstInt 10) (CstInt 32))))
+          @?= ["sum: 42"],
+    --
+      testCase "Print sum of two integers and a boolean" $
+        fst (eval' (Print "sum" (Add (CstInt 10) (CstBool True))))
+          @?= [],
+    --
+      testCase "Print result from If (True)" $
+        fst (eval' (Print "cond" (If (CstBool True) (CstInt 1) (CstInt 0))))
+          @?= ["cond: 1"],
+    --
+      testCase "Print function value" $
+        fst (eval' (Print "fn" (Lambda "x" (Add (Var "x") (CstInt 1)))))
+          @?= ["fn: #<fun>"]
+    --
+    ]
+
+keyValueTests :: TestTree
+keyValueTests =
+  testGroup
+    "Key-Value Store"
+    [ testCase "KvPut and KvGet" $
+        snd (runEval (eval (Let "put" (KvPut (CstInt 1) (CstBool True)) (KvGet (CstInt 1)))))
+          @?= Right (ValBool True),
+      --
+      testCase "KvPut Overwrite and KvGet" $
+        snd (runEval (eval (Let "put1" (KvPut (CstInt 2) (CstInt 42))
+                           (Let "put2" (KvPut (CstInt 2) (CstInt 100)) (KvGet (CstInt 2))))))
+          @?= Right (ValInt 100),
+      --
+      testCase "KvGet Non-Existent Key" $
+        snd (runEval (eval (KvGet (CstInt 3))))
+          @?= Left "KvGet: key not found"
+    ]
+
 tests :: TestTree
 tests = 
   testGroup 
     "APL" 
     [
-      evalTests
+      evalTests,
+      printTest,
+      keyValueTests
     ]
