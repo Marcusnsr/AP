28afe6af068cadd917326509c9a28a3ad650ba92
Marcusnsr <mnsr@hotmail.dk>
2024-09-22 15:19:40 +0200

    done with a2

--- week2/a2-handout/src/APL/Check.hs
+++ week2/a2-handout/src/APL/Check.hs
@@ -1,13 +1,63 @@
 module APL.Check (checkExp, Error) where
 
-import APL.AST (Exp (..), VName)
+import APL.AST (Exp(..), VName)
+import Control.Monad (liftM, ap)
 
 type Error = String
+type Env = [VName]
 
-newtype CheckM a = CheckM () -- TODO - give this a proper definition.
+newtype CheckM a = CheckM { runCheckM :: Env -> Maybe a }
+
+instance Functor CheckM where
+    fmap = liftM
+
+instance Applicative CheckM where
+    pure x = CheckM $ \_ -> Just x
+    (<*>) = ap
+
+instance Monad CheckM where
+    CheckM m >>= f = CheckM $ \env ->
+        case m env of
+            Nothing -> Nothing
+            Just x  -> runCheckM (f x) env
+
+throwError :: CheckM a
+throwError = CheckM $ \_ -> Nothing
+
+askEnv :: CheckM Env
+askEnv = CheckM $ \env -> Just env
+
+extendEnv :: VName -> CheckM a -> CheckM a
+extendEnv v (CheckM m) = CheckM $ \env -> m (v : env)
+
+checkVar :: VName -> CheckM ()
+checkVar v = do
+    env <- askEnv
+    if v `elem` env
+        then return ()
+        else throwError
 
 check :: Exp -> CheckM ()
-check = undefined
+check (CstInt _) = return ()
+check (CstBool _) = return ()
+check (Var v) = checkVar v
+check (Add e1 e2) = check e1 >> check e2
+check (Sub e1 e2) = check e1 >> check e2
+check (Mul e1 e2) = check e1 >> check e2
+check (Div e1 e2) = check e1 >> check e2
+check (Pow e1 e2) = check e1 >> check e2
+check (Eql e1 e2) = check e1 >> check e2
+check (If e1 e2 e3) = check e1 >> check e2 >> check e3
+check (Let v e body) = check e >> extendEnv v (check body)
+check (Lambda v body) = extendEnv v (check body)
+check (Apply e1 e2) = check e1 >> check e2
+check (TryCatch e1 e2) = check e1 >> check e2
+check (Print _ e) = check e
+check (KvPut e1 e2) = check e1 >> check e2
+check (KvGet e) = check e
 
 checkExp :: Exp -> Maybe Error
-checkExp = undefined
+checkExp exp =
+    case runCheckM (check exp) [] of
+        Nothing -> Just "Variable not in scope"
+        Just _  -> Nothing
--- week2/a2-handout/src/APL/Check_Tests.hs
+++ week2/a2-handout/src/APL/Check_Tests.hs
@@ -1,6 +1,6 @@
 module APL.Check_Tests (tests) where
 
-import APL.AST (Exp (..))
+import APL.AST (Exp(..))
 import APL.Check (checkExp)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (assertFailure, testCase, (@?=))
@@ -23,4 +23,24 @@ tests :: TestTree
 tests =
   testGroup
     "Checking"
-    []
+    [ testPos (CstInt 2)  -- Constant should pass
+    , testPos (Lambda "x" (Var "x"))  -- Variable bound by lambda
+    , testPos (Let "x" (CstInt 3) (Var "x"))  -- Variable bound by let
+
+    -- Test variables out of scope
+    , testNeg (Var "x")  -- Unbound variable should fail
+    , testNeg (Apply (Lambda "x" (Var "x")) (Var "y"))  -- 'y' not bound in scope
+
+    -- Test shadowing
+    , testPos (Let "x" (CstInt 3) (Let "x" (CstBool True) (Var "x")))  -- Inner 'x' should shadow outer
+
+    -- Test nested Let
+    , testPos (Let "x" (CstInt 2) (Let "y" (Add (Var "x") (CstInt 3)) (Var "y")))
+    , testNeg (Let "x" (CstInt 2) (Add (Var "y") (CstInt 3)))  -- 'y' is not in scope
+
+    -- Test Let and Apply
+    , testPos (Let "f" (Lambda "x" (Add (Var "x") (CstInt 1))) (Apply (Var "f") (CstInt 2)))
+
+    -- Test failed Let
+    , testNeg (Let "f" (Lambda "x" (Add (Var "x") (Var "y"))) (Apply (Var "f") (CstInt 2)))  -- 'y' is not bound
+    ]
