554b5a152da80b55277017d5847fb633539539c8
Marcusnsr <mnsr@hotmail.dk>
2024-10-10 10:36:48 +0200

    fix shadowing

--- a5/a5-handout/runtests.hs
+++
@@ -1,6 +0,0 @@
-import qualified APL.Tests
-import Test.Tasty (defaultMain)
-import Test.Tasty.QuickCheck (testProperties)
-
-main :: IO ()
-main = defaultMain (testProperties "APL properties" APL.Tests.properties)
